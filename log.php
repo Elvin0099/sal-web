95cfa7e89a6c8215d65f160db4d6baec28b22cb2
Elvin0099 <20.elvinaliyev.08@gmail.com>
2025-05-02 03:07:38 +0400

    log.php

--- log.php
+++ log.php
@@ -1,7 +1,7 @@
 <?php
 $data = json_decode(file_get_contents("php://input"), true);
-if (isset($data["answer"])) {
-    $line = date("Y-m-d H:i:s") . " - Cavab: " . $data["answer"] . "\n";
+if (isset($data["answer"]) && isset($data["name"])) {
+    $line = date("Y-m-d H:i:s") . " - " . $data["name"] . ": " . $data["answer"] . "\n";
     file_put_contents("log.txt", $line, FILE_APPEND);
 }
 ?>
